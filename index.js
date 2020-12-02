a04ed6e6f273264b61dec86ec585bc468f7fd63a
Jalvarado115 <31940182+Jalvarado115@users.noreply.github.com>
2020-12-01 22:36:54 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,81 @@
+const canvas = document.querySelector('canvas');
+
+const context = canvas.getContext('2d');
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+class Player{
+    constructor(x, y, radius, color){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+    }
+    drawPlayer(){
+        context.beginPath();
+        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        context.fillStyle = this.color;
+        context.fill();
+    }
+}
+
+class Projectile{
+    constructor(x, y, radius, color, velocity){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = velocity;
+    }
+    drawProjectile(){
+        context.beginPath();
+        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        context.fillStyle = this.color;
+        context.fill();
+    }
+
+    update(){
+        this.drawProjectile();
+        this.x = this.x + this.velocity.x;
+        this.y = this.y + this.velocity.y;
+    }
+}
+
+const x = canvas.width / 2;
+const y = canvas.height / 2;
+
+const player = new Player(x, y, 30, 'blue')
+
+const projectiles = [];
+
+function animate(){
+    requestAnimationFrame(animate);
+    context.clearRect(0, 0, canvas.width, canvas.height)
+    player.drawPlayer();
+    projectiles.forEach((projectile) => {
+       projectile.update();
+    });
+}
+
+addEventListener('click', (event) => {
+    const angle = Math.atan2(
+        event.clientY - canvas.height / 2,
+        event.clientX - canvas.width / 2
+        );
+
+    const velocity = {
+        x: Math.cos(angle),
+        y: Math.sin(angle)
+    }
+    
+    projectiles.push(new Projectile(
+        canvas.width / 2,
+        canvas.height / 2,
+        5,
+        'red',
+        velocity
+        ));
+});
+
+animate();
