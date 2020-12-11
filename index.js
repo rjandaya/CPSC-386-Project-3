d99e612b09e27d8ed81257eed45cb6eab7fd87ed
RJ Andaya <rjandaya98@gmail.com>
2020-12-10 21:31:47 -0800

    Finalize Base Code

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ canvas.width = innerWidth
 canvas.height = innerHeight
 
 const scoreEl = document.querySelector('#scoreEl')
+const startGameBtn = document.querySelector('#startGameBtn')
+
+const modalEl = document.querySelector('#modalEl')
+
+const bigScoreEl = document.querySelector('#bigScoreEl')
 
 class Player {
     constructor(x, y, radius, color) {
@@ -101,11 +106,24 @@ class Particle {
 const x = canvas.width / 2
 const y = canvas.height / 2
 
-const player = new Player(x, y, 10, 'white')
+let player = new Player(x, y, 10, 'white')
 
-const projectiles = []
-const enemies = []
-const particles = []
+let projectiles = []
+let enemies = []
+let particles = []
+
+function init() {
+    player = new Player(x, y, 10, 'white')
+
+    projectiles = []
+    enemies = []
+    particles = []
+
+    score = 0
+
+    scoreEl.innerHTML = score
+    bigScoreEl.innerHTML = score
+}
 
 function spawnEnemies() {
   setInterval(() => {
@@ -175,6 +193,8 @@ function animate() {
       if (dist - enemy.radius - player.radius < 1)
           {
               cancelAnimationFrame(animationId)
+              modalEl.style.display = 'flex'
+              bigScoreEl.innerHTML = score
           }
 
 
@@ -240,5 +260,12 @@ addEventListener('click', (event) => {
         ))
 })
 
-animate()
-spawnEnemies()
+startGameBtn.addEventListener('click', () => {
+    init()
+    animate()
+    spawnEnemies()
+    modalEl.style.display = 'none'
+})
+
+
+
